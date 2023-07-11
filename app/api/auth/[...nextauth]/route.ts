75def0e5a563fbf358c117d0babc473478606b64
Stepan Jamalyan <stepan.jamalyan@gmail.com>
2023-07-11 13:03:21 +0600

    added merging user cart with anonymous cart

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -5,14 +5,28 @@ import { prisma } from "@/lib/db/prisma"
 import { Adapter } from "next-auth/adapters"
 import GoogleProvider from "next-auth/providers/google"
 import NextAuth from "next-auth/next"
+import { mergeAnonymousCartIntoUserCart } from "@/lib/db/cart"
 
 
 export const authOptions: NextAuthOptions = {
     adapter: PrismaAdapter(prisma) as Adapter,
-    providers: [GoogleProvider({
-        clientId: env.GOOGLE_CLIENT_ID,
-        clientSecret: env.GOOGLE_CLIENT_SECRET
-    })]
+    providers: [
+        GoogleProvider({
+            clientId: env.GOOGLE_CLIENT_ID,
+            clientSecret: env.GOOGLE_CLIENT_SECRET
+        })
+    ],
+    callbacks: {
+        session({ session, user }) {
+            session.user.id = user.id
+            return session
+        },
+    },
+    events: {
+        async signIn({ user }) {
+            await mergeAnonymousCartIntoUserCart(user.id)
+        }
+    }
 }
 
 const handler = NextAuth(authOptions)
